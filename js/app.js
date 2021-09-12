5f371032a5d633fa0584b6e1b3fd688aa18ad0a5
Md Shah Jalal <bestwebdesignerr@gmail.com>
2021-09-13 03:40:04 +0600

    custom style added

--- js/app.js
+++ js/app.js
@@ -20,8 +20,8 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn-ranga-add">add to cart</button>
+      <button id="details-btn" class="btn-ranga-details">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
